837f1571b14113dce659de0d77f37ec24a469577
Mark Wright <markscottwright@gmail.com>
2017-04-27 22:55:29 -0400

    Challenge8

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,21 @@ mod challenge3;
 mod challenge4;
 mod challenge6;
 mod challenge7;
+mod challenge8;
 
 extern crate base64;
 extern crate crypto;
 
+use std::time::{Instant, Duration};
+
 macro_rules! challenge {
     ($x:ident) => (
         println!("===== begin: {} =====", stringify!($x));
+        let start = Instant::now();
         $x::$x();
+        let stop = start.elapsed();
+        println!("{} seconds",
+            stop.as_secs() as f32 + (stop.subsec_nanos() as f32 / 1_000_000_000f32));
         println!("===== end:   {} =====", stringify!($x));
         );
 }
@@ -22,4 +29,5 @@ fn main() {
     challenge!(challenge4);
     challenge!(challenge6);
     challenge!(challenge7);
+    challenge!(challenge8);
 }
