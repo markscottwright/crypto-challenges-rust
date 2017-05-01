da63d97e5e252504d4c9f5b0cfc49b2714756da7
Mark Wright <mwright@cygnacom.com>
2017-05-01 13:03:24 -0400

    do lower case first

--- src/challenge12.rs
+++ src/challenge12.rs
@@ -134,7 +134,7 @@ pub fn crack_ecb_with_known_blocksize_and_suffix
             let match_ciphertext_block = &oracle_fn(&match_block).unwrap()[compare_range.clone()];
 
             // try ascii first...
-            for b in (65..123).chain((0..65)).chain(123..255) {
+            for b in (97..123).chain((65..97)).chain((0..65)).chain(123..255) {
                 *(test_block.last_mut().unwrap()) = b;
                 let test_ciphertext_block = &oracle_fn(&test_block).unwrap()[compare_range.clone()];
                 if test_ciphertext_block == match_ciphertext_block {
