1a407b2b3df25f4fb0d6ab255627b0b91501f763
Mark Wright <mwright@cygnacom.com>
2017-05-01 11:02:18 -0400

    Challenge 12 - first block

--- src/challenge12.rs
+++ src/challenge12.rs
@@ -62,10 +62,88 @@ pub fn is_ecb(oracle_fn: fn(&[u8]) -> Result<Vec<u8>, symmetriccipher::Symmetric
     ciphertext[0..blocksize] == ciphertext[blocksize..blocksize * 2]
 }
 
+pub fn length_of_suffix(oracle_fn: fn(&[u8])
+                                      -> Result<Vec<u8>, symmetriccipher::SymmetricCipherError>,
+                        blocksize: usize)
+                        -> usize {
+
+    let mut last_cipher_len: Option<usize> = None;
+
+    // keep adding to cleartext size until we overflow a block
+    for i in 0..blocksize {
+        let cur_cipher_len = oracle_fn(&vec![1u8;i]).unwrap().len();
+        let overflowed = match last_cipher_len {
+            None => {
+                last_cipher_len = Some(cur_cipher_len);
+                false
+            }
+            Some(x) if x == cur_cipher_len => false,
+            _ => true,
+        };
+
+        // need to add i to make it overflow, so the suffix's length (mod
+        // blocksize) is blocksize - (i - 1)
+        if overflowed {
+            let suffix_mod_blocksize = blocksize - (i - 1);
+
+            // last_cipher_len was suffix + padding, so round down to nearest
+            // blocksize and then add the extra suffix length
+            let suffix_len = last_cipher_len.unwrap() - blocksize + suffix_mod_blocksize;
+            return suffix_len;
+        }
+    }
+
+    panic!("Unexpected error - never found suffix length!")
+}
+
+pub fn crack_ecb_with_known_blocksize_and_suffix
+    (oracle_fn: fn(&[u8]) -> Result<Vec<u8>, symmetriccipher::SymmetricCipherError>,
+     blocksize: usize,
+     suffix_len: usize)
+     -> Result<Vec<u8>, symmetriccipher::SymmetricCipherError> {
+
+    let mut decrypted_suffix = Vec::with_capacity(suffix_len);
+
+    for _ in 0..blocksize {
+
+        // match block, 0's 
+        // test block,  0's || suffix_so_far || 0..ff
+        let short_block_len = blocksize - decrypted_suffix.len() - 1;
+        let mut match_block = Vec::with_capacity(blocksize);
+        let mut test_block =  Vec::with_capacity(blocksize);
+        for _ in 0..short_block_len {
+            match_block.push(0);
+            test_block.push(0);
+        }
+        for b in decrypted_suffix.iter().cloned() {
+            test_block.push(b);
+        }
+        test_block.push(0);
+
+        // Keep varying the last byte until the enciphered block matches the
+        // enciphered block from the one-byte-short match block.  Then we know what
+        // the first byte is in the suffix.
+        let match_ciphertext_block = &oracle_fn(&match_block).unwrap()[0..blocksize];
+        for b in 0..0xff {
+            test_block[blocksize-1] = b;
+            let test_ciphertext_block = &oracle_fn(&test_block).unwrap()[0..blocksize];
+            if test_ciphertext_block == match_ciphertext_block {
+                decrypted_suffix.push(b);
+                break;
+            }
+        }
+    }
+
+    Ok(decrypted_suffix)
+}
+
 pub fn challenge12() {
     let blocksize = find_blocksize(oracle);
     let using_ecb = is_ecb(oracle, blocksize);
+    let suffix_length = length_of_suffix(oracle, blocksize);
+    let suffix = crack_ecb_with_known_blocksize_and_suffix(oracle, blocksize, suffix_length);
     print!("blocksize = {} ", blocksize);
     println!("is ecb {}", using_ecb);
-
+    println!("suffix len {}", suffix_length);
+    println!("suffix {}", String::from_utf8_lossy(&suffix.unwrap()));
 }
