33248ee439ad440e3f1c0a6373e114fccb066f15
Mark Wright <mwright@cygnacom.com>
2017-07-26 09:48:04 -0400

    Reformat

--- src/challenge20.rs
+++ src/challenge20.rs
@@ -3,35 +3,21 @@ use aes::encrypt_ctr;
 use base64::decode;
 use challenge6::transpose;
 
-pub fn shortest_vec(vecs: &Vec<Vec<u8>>) -> usize {
-    let mut min_len = vecs[0].len();
-    let mut shortest_index = 0;
-
-    for i in 1..vecs.len() {
-        if vecs[i].len() < min_len {
-            min_len = vecs[i].len();
-            shortest_index = i;
-        }
-    }
-
-    shortest_index
-}
-
 pub fn solve_repeated_xor(ciphertext: &[u8], keysize: usize) -> Vec<u8> {
-    // copy and paste from challenge6.  Bad programmer...
-
     let mut key = Vec::new();
     for c in transpose(&ciphertext, keysize) {
         let (k, _, _) = most_english_xor(&c).unwrap();
         key.push(k);
     }
-
-    {
-        let keystr = String::from_utf8_lossy(&key);
-        println!("{}", keystr);
-    }
-
     key
+}
+
+fn concat_prefixes(vecs: &Vec<Vec<u8>>, prefix_len: usize) -> Vec<u8> {
+    let mut concatted: Vec<u8> = Vec::new();
+    for c in vecs {
+        concatted.extend(c[0..prefix_len].iter());
+    }
+    concatted
 }
 
 // Note that this is essentially the same as challenge 6.
@@ -47,17 +33,12 @@ pub fn challenge20() {
         .map(|cleartext| encrypt_ctr(cleartext, key, &nonce).unwrap())
         .collect::<Vec<_>>();
 
-    let shortest_ciphertext_index = shortest_vec(&ciphertexts);
-    let shortest_ciphertext_len = ciphertexts[shortest_ciphertext_index].len();
-    let mut concatted_ciphertexts: Vec<u8> = Vec::new();
-    {
-        for c in &ciphertexts {
-            concatted_ciphertexts.extend(c[0..shortest_ciphertext_len].iter());
-        }
-    }
-
+    let shortest_ciphertext_len = ciphertexts.iter().map(|c| c.len()).min().unwrap();
+    let concatted_ciphertexts = concat_prefixes(&ciphertexts, shortest_ciphertext_len);
     let key = solve_repeated_xor(&concatted_ciphertexts, shortest_ciphertext_len);
+
     for c in &ciphertexts {
-        println!("{}", String::from_utf8_lossy(&repeat_xor(&c, &key)[0..shortest_ciphertext_len]));
+        println!("{}",
+                 String::from_utf8_lossy(&repeat_xor(&c, &key)[0..shortest_ciphertext_len]));
     }
 }
