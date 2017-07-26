222c143348298df261416c5998f0a294601a01bb
Mark Wright <mwright@cygnacom.com>
2017-07-26 09:37:53 -0400

    Challenge 20

--- src/challenge20.rs
+++ src/challenge20.rs
@@ -1,5 +1,7 @@
+use bytes::*;
 use aes::encrypt_ctr;
 use base64::decode;
+use challenge6::transpose;
 
 pub fn shortest_vec(vecs: &Vec<Vec<u8>>) -> usize {
     let mut min_len = vecs[0].len();
@@ -15,6 +17,24 @@ pub fn shortest_vec(vecs: &Vec<Vec<u8>>) -> usize {
     shortest_index
 }
 
+pub fn solve_repeated_xor(ciphertext: &[u8], keysize: usize) -> Vec<u8> {
+    // copy and paste from challenge6.  Bad programmer...
+
+    let mut key = Vec::new();
+    for c in transpose(&ciphertext, keysize) {
+        let (k, _, _) = most_english_xor(&c).unwrap();
+        key.push(k);
+    }
+
+    {
+        let keystr = String::from_utf8_lossy(&key);
+        println!("{}", keystr);
+    }
+
+    key
+}
+
+// Note that this is essentially the same as challenge 6.
 pub fn challenge20() {
     let key = b"YELLOW SUBMARINE";
     let nonce = 0;
@@ -28,4 +48,16 @@ pub fn challenge20() {
         .collect::<Vec<_>>();
 
     let shortest_ciphertext_index = shortest_vec(&ciphertexts);
+    let shortest_ciphertext_len = ciphertexts[shortest_ciphertext_index].len();
+    let mut concatted_ciphertexts: Vec<u8> = Vec::new();
+    {
+        for c in &ciphertexts {
+            concatted_ciphertexts.extend(c[0..shortest_ciphertext_len].iter());
+        }
+    }
+
+    let key = solve_repeated_xor(&concatted_ciphertexts, shortest_ciphertext_len);
+    for c in &ciphertexts {
+        println!("{}", String::from_utf8_lossy(&repeat_xor(&c, &key)[0..shortest_ciphertext_len]));
+    }
 }
